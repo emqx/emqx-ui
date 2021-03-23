45395476cf95dd1f78a8c032fb2daaed0a8e19f5
Kinplemelon <wujiu529@gmail.com>
2021-03-23 22:26:45 +0800

    feat(form-item): add operation slot

--- packages/FormItem/src/form-item.vue
+++ packages/FormItem/src/form-item.vue
@@ -1,6 +1,12 @@
 <template>
-  <el-form-item class="emqx-form-item">
-    <template v-if="$slots.label" #label>
+  <el-form-item class="emqx-form-item" :class="$slots.label && $slots.operation ? 'with-operation' : ''">
+    <template v-if="$slots.label && $slots.operation" #label>
+      <slot name="label"></slot>
+      <div class="float-right">
+        <slot name="operation"></slot>
+      </div>
+    </template>
+    <template v-else-if="$slots.label" #label>
       <slot name="label"></slot>
     </template>
     <slot></slot>
@@ -16,7 +22,30 @@ export default defineComponent({
   components: {
     ElFormItem,
   },
+  props: {
+    hasOperation: {
+      type: Boolean,
+    },
+  },
 })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.emqx-form-item {
+  &.with-operation {
+    .el-form-item__label {
+      width: 100%;
+      padding-right: 0;
+      text-align: left;
+      &::after {
+        content: '';
+        display: block;
+        clear: both;
+      }
+    }
+    .float-right {
+      float: right;
+    }
+  }
+}
+</style>
