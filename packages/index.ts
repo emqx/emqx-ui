22618ed184b0df70ff87d0b4bad1532bc6197fc2
Kinplemelon <wujiu529@gmail.com>
2021-03-30 17:14:31 +0800

    feat(global): set default language

--- packages/index.ts
+++ packages/index.ts
@@ -28,7 +28,11 @@ import Upload from './Upload'
 import Tags from './Tags'
 import KeyValueEditor from './KeyValueEditor'
 import './style/global.scss'
+
+import lang from 'element-plus/lib/locale/lang/zh-cn'
 import locale from 'element-plus/lib/locale'
+
+locale.use(lang)
 
 interface VueWindow extends Window {
   Vue?: App
