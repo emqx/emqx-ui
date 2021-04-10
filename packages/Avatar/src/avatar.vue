eb5a46d627288501cfebac13aaa1a280dc2ba538
Kinplemelon <wujiu529@gmail.com>
2021-04-10 17:16:59 +0800

    feat(avatar): add avatar

---
+++ packages/Avatar/src/avatar.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-avatar class="emqx-avatar" v-if="!$slots.menu">
+    <slot></slot>
+  </el-avatar>
+  <el-popover v-else popper-class="emqx-avatar-popover" placement="bottom-end" trigger="click">
+    <template #reference>
+      <el-avatar class="emqx-avatar" v-bind="$attrs">
+        <slot></slot>
+      </el-avatar>
+    </template>
+    <slot name="menu"></slot>
+  </el-popover>
+</template>
+
+<script>
+import { ElAvatar, ElPopover } from 'element-plus'
+
+export default {
+  name: 'EmqxAvatar',
+  components: { ElAvatar, ElPopover },
+}
+</script>
