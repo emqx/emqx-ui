e6db215fcf73acaf0db69f5d165da47d731357b3
Kinplemelon <wujiu529@gmail.com>
2021-05-17 09:54:30 +0800

    feat(gloabl): add radio-group component

---
+++ packages/RadioGroup/src/radio-group.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-radio-group>
+    <slot></slot>
+  </el-radio-group>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ElRadioGroup } from 'element-plus'
+
+export default defineComponent({
+  name: 'EmqxRadioGroup',
+  components: { ElRadioGroup },
+})
+</script>
