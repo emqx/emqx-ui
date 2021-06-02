fafd9ae315e6865235194552349049619605a84d
YuShifan <894402575bt@gmail.com>
2021-06-02 15:14:34 +0800

    style(input): add input number scss file

--- packages/InputNumber/src/input-number.vue
+++ packages/InputNumber/src/input-number.vue
@@ -11,32 +11,3 @@ export default defineComponent({
   components: { ElInputNumber },
 })
 </script>
-
-<style lang="scss">
-.emqx-input-number {
-  $border-radius: 8px;
-  &.el-input-number {
-    width: 100%;
-    .el-input__inner {
-      text-align: left;
-    }
-    &.is-controls-right {
-      .el-input-number__increase {
-        border-radius: 0 $border-radius 0 0;
-      }
-      .el-input-number__decrease {
-        border-radius: 0 0 $border-radius 0;
-      }
-    }
-  }
-  .el-input__inner {
-    border-radius: $border-radius;
-  }
-  .el-input-number__decrease {
-    border-radius: $border-radius 0 0 $border-radius;
-  }
-  .el-input-number__increase {
-    border-radius: 0 $border-radius $border-radius 0;
-  }
-}
-</style>
