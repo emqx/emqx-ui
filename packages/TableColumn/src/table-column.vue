ec4cd29f022633252eafdd2c85f03c1d610f223f
Kinplemelons <wujiu529@gmail.com>
2021-06-24 14:38:21 +0800

    feat(table column): add header slot

--- packages/TableColumn/src/table-column.vue
+++ packages/TableColumn/src/table-column.vue
@@ -1,5 +1,8 @@
 <template>
   <el-table-column class="emqx-table-column">
+    <template v-if="$slots.header" #header="scope">
+      <slot name="header" :column="scope.column" :index="scope.$index"></slot>
+    </template>
     <template v-if="$slots.default" #default="scope">
       <slot :row="scope.row" :index="scope.$index"></slot>
     </template>
