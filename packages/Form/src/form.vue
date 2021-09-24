f39395fb482fa46158932802f3d00a9c047563ec
Kinplemelon <wujiu529@gmail.com>
2021-09-24 14:27:19 +0800

    fix(form): error func of reset fields

--- packages/Form/src/form.vue
+++ packages/Form/src/form.vue
@@ -19,7 +19,7 @@ export default defineComponent({
       return form.value.validate()
     }
     const resetField = () => {
-      form.value.resetField()
+      form.value.resetFields()
     }
     return {
       form,
